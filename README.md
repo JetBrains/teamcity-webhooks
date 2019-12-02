419b61951cd131a077b89d302a64dc4de3b68d00
Emma Starukhina <emma.starukhina@jetbrains.com>
2019-12-02 10:54:47 +0100

    Add BUILD_TYPE_ADDED_TO_QUEUE, BUILD_PROBLEMS_CHANGED events support

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # teamcity-webhooks
 
-Short project description goes here.
+TeamCity server-side plugin that allows you to send WebHooks asynchronously to third-party systems.
 
 ## Getting Started
 
@@ -14,18 +14,37 @@ HTTPS clone URL: https://git.jetbrains.team/teamcity-webhooks.git
 
 These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
 
-## Prerequisites
+## Build
 
-What things you need to install the software and how to install them.
+ Issue 'mvn package' command from the root project to build the plugin. Resulting package teamcity-webhooks.zip will be placed in 'target' directory. 
 
 ```
-Examples
+mvn package
 ```
 
-## Deployment
+## Install
 
-Add additional notes about how to deploy this on a production system.
+ To install the plugin, put zip archive to 'plugins' dir under TeamCity data directory and restart the server. 
 
-## Resources
+## Configuration
 
-Add links to external resources for this project, such as CI server, bug tracker, etc.
+ The plugin can be configured via following project parameters (for agent events ```<Root Project>``` should be used):
+ -  ```teamcity.internal.webhooks.enable ```   - flag indicates web hooks functionality is enabled.
+        Possible values: ```true```, ```false```.
+ -  ``` teamcity.internal.webhooks.events ``` - list of events whose occurrence will trigger web hooks sending.
+        Possible values: ```AGENT_REGISTRED```, ```AGENT_UNREGISTERED```, ```AGENT_REMOVED```, ```BUILD_STARTED```, ```BUILD_FINISHED```, ```BUILD_INTERRUPTED```, ```CHANGES_LOADED```, ```BUILD_TYPE_ADDED_TO_QUEUE```, ```BUILD_PROBLEMS_CHANGED```. 
+        Should be separated by ```;```.
+ - ```teamcity.internal.webhooks.url``` : url to send web hooks by HTTP POST request
+ - ```teamcity.internal.webhooks.{event_name}.fields ```: list of fields are supposed to be return in web hook for specific event in Team City REST-API format.
+        Example value:  
+       
+       teamcity.internal.webhooks.BUILD_STARTED.fields =
+               fields=id,buildTypeId,number,running-info(percentageComplete,elapsedSeconds,estimatedTotalSeconds,leftSeconds,currentStageText)
+                       
+   ####Http Basic Auth parameters:
+ -  ```teamcity.internal.webhooks.username ``` - username.
+ -  ```teamcity.internal.webhooks.password ``` - password (keep in password type parameter).
+    
+    ####Advanced parameters:
+ - ```teamcity.internal.webhooks.retry_count``` - count of retry which will be performed in case of exception thrown during the web hook request or unsuccessful HTTP response code (not 2**)
+        Default value is 0.
--- teamcity-webhook-server/src/main/java/jetbrains/buildServer/webhook/WebhooksManager.java
+++ teamcity-webhook-server/src/main/java/jetbrains/buildServer/webhook/WebhooksManager.java
@@ -1,5 +1,6 @@
 package jetbrains.buildServer.webhook;
 
+import jetbrains.buildServer.BuildProblemData;
 import jetbrains.buildServer.plugins.PluginLifecycleListenerAdapter;
 import jetbrains.buildServer.plugins.impl.PluginLifecycleEventDispatcher;
 import jetbrains.buildServer.serverSide.*;
@@ -9,6 +10,8 @@ import jetbrains.buildServer.webhook.async.events.AsyncEvent;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Component;
 import java.util.Arrays;
+import java.util.List;
+
 import static jetbrains.buildServer.webhook.WebhooksManager.EventNames.*;
 
 @Component
@@ -22,6 +25,8 @@ public class WebhooksManager {
         static final String BUILD_FINISHED = "BUILD_FINISHED";
         static final String BUILD_INTERRUPTED = "BUILD_INTERRUPTED";
         static final String CHANGES_LOADED = "CHANGES_LOADED";
+        static final String BUILD_TYPE_ADDED_TO_QUEUE = "BUILD_TYPE_ADDED_TO_QUEUE";
+        static final String BUILD_PROBLEMS_CHANGED = "BUILD_PROBLEMS_CHANGED";
     }
 
     public WebhooksManager(PluginLifecycleEventDispatcher dispatcher,
@@ -30,7 +35,7 @@ public class WebhooksManager {
                            WebhooksEventListener eventListener) {
 
         asyncEventDispatcher.subscribe(Arrays.asList(AGENT_REGISTRED, AGENT_UNREGISTERED, AGENT_REMOVED
-                , BUILD_STARTED, BUILD_FINISHED, BUILD_INTERRUPTED, CHANGES_LOADED), eventListener);
+                , BUILD_STARTED, BUILD_FINISHED, BUILD_INTERRUPTED, CHANGES_LOADED, BUILD_TYPE_ADDED_TO_QUEUE, BUILD_PROBLEMS_CHANGED), eventListener);
 
         dispatcher.addListener(new PluginLifecycleListenerAdapter() {
             @Override
@@ -56,6 +61,12 @@ public class WebhooksManager {
             }
 
             @Override
+            public void buildTypeAddedToQueue(@NotNull final SQueuedBuild queuedBuild) {
+                final BuildPromotion buildPromotion = queuedBuild.getBuildPromotion();
+                asyncEventDispatcher.publish(new AsyncEvent(BUILD_TYPE_ADDED_TO_QUEUE, buildPromotion.getId(), buildPromotion.getProjectId()));
+            }
+
+            @Override
             public void buildStarted(@NotNull SRunningBuild build) {
                 asyncEventDispatcher.publish(new AsyncEvent(BUILD_STARTED, build.getBuildPromotion().getId(), build.getProjectId()));
             }
@@ -73,6 +84,13 @@ public class WebhooksManager {
             @Override
             public void changesLoaded(@NotNull SRunningBuild build) {
                 asyncEventDispatcher.publish(new AsyncEvent(CHANGES_LOADED, build.getBuildPromotion().getId(), build.getProjectId()));
+            }
+
+            @Override
+            public void buildProblemsChanged(@NotNull final SBuild build,
+                                             @NotNull final List<BuildProblemData> before,
+                                             @NotNull final List<BuildProblemData> after) {
+                asyncEventDispatcher.publish(new AsyncEvent(BUILD_PROBLEMS_CHANGED, build.getBuildPromotion().getId(), build.getProjectId()));
             }
         });
     }
