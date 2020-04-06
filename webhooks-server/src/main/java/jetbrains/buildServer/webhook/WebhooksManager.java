68861e8721a5346958f4c7f4666a6bae43d9ef9f
Emma Starukhina <emma.starukhina@jetbrains.com>
2020-04-06 15:28:00 +0200

    TW-65278 Rename webhook plugin

--- webhooks-server/src/main/java/jetbrains/buildServer/webhook/WebhooksManager.java
+++ webhooks-server/src/main/java/jetbrains/buildServer/webhook/WebhooksManager.java
@@ -20,18 +20,18 @@ import static jetbrains.buildServer.webhook.WebhooksManager.EventNames.*;
 public class WebhooksManager {
 
     public static final class EventNames {
-        static final String AGENT_REGISTRED = "AGENT_REGISTRED";
-        static final String AGENT_UNREGISTERED = "AGENT_UNREGISTERED";
-        static final String AGENT_REMOVED = "AGENT_REMOVED";
-        static final String BUILD_STARTED = "BUILD_STARTED";
-        static final String BUILD_FINISHED = "BUILD_FINISHED";
-        static final String BUILD_INTERRUPTED = "BUILD_INTERRUPTED";
-        static final String CHANGES_LOADED = "CHANGES_LOADED";
-        static final String BUILD_TYPE_ADDED_TO_QUEUE = "BUILD_TYPE_ADDED_TO_QUEUE";
-        static final String BUILD_REMOVED_FROM_QUEUE = "BUILD_REMOVED_FROM_QUEUE";
-        static final String BUILD_PROBLEMS_CHANGED = "BUILD_PROBLEMS_CHANGED";
-        static final String MARKED_AS_SUCCESSFUL = "MARKED_AS_SUCCESSFUL";
-        static final String FAILURE_DETECTED = "FAILURE_DETECTED";
+        public static final String AGENT_REGISTRED = "AGENT_REGISTRED";
+        public static final String AGENT_UNREGISTERED = "AGENT_UNREGISTERED";
+        public static final String AGENT_REMOVED = "AGENT_REMOVED";
+        public static final String BUILD_STARTED = "BUILD_STARTED";
+        public static final String BUILD_FINISHED = "BUILD_FINISHED";
+        public static final String BUILD_INTERRUPTED = "BUILD_INTERRUPTED";
+        public static final String CHANGES_LOADED = "CHANGES_LOADED";
+        public static final String BUILD_TYPE_ADDED_TO_QUEUE = "BUILD_TYPE_ADDED_TO_QUEUE";
+        public static final String BUILD_REMOVED_FROM_QUEUE = "BUILD_REMOVED_FROM_QUEUE";
+        public static final String BUILD_PROBLEMS_CHANGED = "BUILD_PROBLEMS_CHANGED";
+        public static final String MARKED_AS_SUCCESSFUL = "MARKED_AS_SUCCESSFUL";
+        public static final String FAILURE_DETECTED = "FAILURE_DETECTED";
     }
 
     public WebhooksManager(PluginLifecycleEventDispatcher dispatcher,
