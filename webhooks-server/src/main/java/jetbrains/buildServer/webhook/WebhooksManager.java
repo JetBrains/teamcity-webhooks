333a8633d54ceda12c5bd9187e56a68f6accf32a
Maxim Zaytsev <maxim.zaytsev@jetbrains.com>
2022-10-21 12:56:21 +0200

    use classes from core instead of web hooks plugin classes Maxim Zaytsev Yesterday 14:30

--- webhooks-server/src/main/java/jetbrains/buildServer/webhook/WebhooksManager.java
+++ webhooks-server/src/main/java/jetbrains/buildServer/webhook/WebhooksManager.java
@@ -1,18 +1,10 @@
 package jetbrains.buildServer.webhook;
 
-import jetbrains.buildServer.BuildProblemData;
-import jetbrains.buildServer.messages.Status;
+import java.util.Arrays;
 import jetbrains.buildServer.plugins.PluginLifecycleListenerAdapter;
 import jetbrains.buildServer.plugins.impl.PluginLifecycleEventDispatcher;
-import jetbrains.buildServer.serverSide.*;
-import jetbrains.buildServer.users.User;
-import jetbrains.buildServer.util.EventDispatcher;
 import jetbrains.buildServer.webhook.async.AsyncEventDispatcher;
-import jetbrains.buildServer.webhook.async.events.AsyncEvent;
-import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Component;
-import java.util.Arrays;
-import java.util.List;
 
 import static jetbrains.buildServer.webhook.WebhooksManager.EventNames.*;
 
@@ -35,7 +27,6 @@ public class WebhooksManager {
     }
 
     public WebhooksManager(PluginLifecycleEventDispatcher dispatcher,
-                           EventDispatcher<BuildServerListener> serverEventDispatcher,
                            AsyncEventDispatcher asyncEventDispatcher,
                            WebhooksEventListener eventListener) {
 
@@ -46,76 +37,6 @@ public class WebhooksManager {
             @Override
             public void beforePluginUnloaded() {
                 asyncEventDispatcher.unsubscribe(eventListener);
-            }
-        });
-
-        serverEventDispatcher.addListener(new BuildServerAdapter() {
-            @Override
-            public void agentRegistered(@NotNull SBuildAgent agent, final long currentlyRunningBuildId) {
-                asyncEventDispatcher.publish(new AsyncEvent(AGENT_REGISTRED, (long)agent.getId()));
-            }
-
-            @Override
-            public void agentUnregistered(@NotNull SBuildAgent agent) {
-                asyncEventDispatcher.publish(new AsyncEvent(AGENT_UNREGISTERED, (long)agent.getId()));
-            }
-
-            @Override
-            public void agentRemoved(@NotNull SBuildAgent agent) {
-                asyncEventDispatcher.publish(new AsyncEvent(AGENT_REMOVED, (long)agent.getId()));
-            }
-            
-            @Override
-            public void buildTypeAddedToQueue(@NotNull final SQueuedBuild queuedBuild) {
-                final BuildPromotion buildPromotion = queuedBuild.getBuildPromotion();
-                asyncEventDispatcher.publish(new AsyncEvent(BUILD_TYPE_ADDED_TO_QUEUE, buildPromotion.getId(), buildPromotion.getProjectId()));
-            }
-
-            public void buildRemovedFromQueue(@NotNull SQueuedBuild queuedBuild,
-                                              final User user,
-                                              final String comment) {
-                final BuildPromotion buildPromotion = queuedBuild.getBuildPromotion();
-                asyncEventDispatcher.publish(new AsyncEvent(BUILD_REMOVED_FROM_QUEUE, buildPromotion.getId(), buildPromotion.getProjectId()));
-            }
-
-            @Override
-            public void buildStarted(@NotNull SRunningBuild build) {
-                asyncEventDispatcher.publish(new AsyncEvent(BUILD_STARTED, build.getBuildPromotion().getId(), build.getProjectId()));
-            }
-
-            @Override
-            public void buildFinished(@NotNull SRunningBuild build) {
-                asyncEventDispatcher.publish(new AsyncEvent(BUILD_FINISHED, build.getBuildPromotion().getId(), build.getProjectId()));
-            }
-
-            @Override
-            public void buildInterrupted(@NotNull SRunningBuild build) {
-                asyncEventDispatcher.publish(new AsyncEvent(BUILD_INTERRUPTED, build.getBuildPromotion().getId(), build.getProjectId()));
-            }
-
-            @Override
-            public void changesLoaded(@NotNull SRunningBuild build) {
-                asyncEventDispatcher.publish(new AsyncEvent(CHANGES_LOADED, build.getBuildPromotion().getId(), build.getProjectId()));
-            }
-
-            @Override
-            public void buildProblemsChanged(@NotNull final SBuild build,
-                                             @NotNull final List<BuildProblemData> before,
-                                             @NotNull final List<BuildProblemData> after) {
-                asyncEventDispatcher.publish(new AsyncEvent(BUILD_PROBLEMS_CHANGED, build.getBuildPromotion().getId(), build.getProjectId()));
-                if (!before.isEmpty() && after.isEmpty()) {
-                    asyncEventDispatcher.publish(new AsyncEvent(MARKED_AS_SUCCESSFUL, build.getBuildPromotion().getId(), build.getProjectId()));
-                }
-                if (before.isEmpty() && !after.isEmpty() && build.isFinished()) {
-                    asyncEventDispatcher.publish(new AsyncEvent(FAILURE_DETECTED, build.getBuildPromotion().getId(), build.getProjectId()));
-                }
-            }
-
-            @Override
-            public void buildChangedStatus(@NotNull final SRunningBuild build, Status oldStatus, Status newStatus) {
-                if (oldStatus.isFailed() || !newStatus.isFailed()) // we are supposed to report failures only
-                    return;
-                asyncEventDispatcher.publish(new AsyncEvent(FAILURE_DETECTED, build.getBuildPromotion().getId(), build.getProjectId()));
             }
         });
     }
--- webhooks-server/src/main/java/jetbrains/buildServer/webhook/async/AsyncEventDispatcher.java
+++ webhooks-server/src/main/java/jetbrains/buildServer/webhook/async/AsyncEventDispatcher.java
@@ -5,10 +5,11 @@ import jetbrains.buildServer.messages.XStreamHolder;
 import jetbrains.buildServer.serverSide.BuildServerAdapter;
 import jetbrains.buildServer.serverSide.BuildServerListener;
 import jetbrains.buildServer.serverSide.ServerPaths;
+import jetbrains.buildServer.serverSide.impl.events.async.AsyncEvent;
 import jetbrains.buildServer.util.EventDispatcher;
-import jetbrains.buildServer.webhook.async.events.AsyncEvent;
 import jetbrains.buildServer.xstream.XStreamFile;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import java.io.*;
@@ -28,151 +29,63 @@ import static java.lang.String.format;
 @Component
 public class AsyncEventDispatcher {
 
-    private static final Logger LOG = Logger.getInstance(AsyncEventDispatcher.class.getName());
-    public static final String WEBHOOKS_UNPROCESSED_ASYNC_EVENTS_FILE = "/webhooks/unprocessedAsyncEvents.bak";
+    private final jetbrains.buildServer.serverSide.impl.events.async.AsyncEventDispatcher myDelegate;
 
-    private final Map<String, List<AsyncEventListener>> listeners = new ConcurrentHashMap<>();
-    private final OrderedExecutor<AsyncEventHandlingTask> orderedExecutor;
-    private final Path unprocessedEventsFilePath;
-
-    public AsyncEventDispatcher(EventDispatcher<BuildServerListener> serverEventDispatcher,
-                                ServerPaths serverPaths) {
-        unprocessedEventsFilePath = Paths.get(serverPaths.getPluginDataDirectory() + WEBHOOKS_UNPROCESSED_ASYNC_EVENTS_FILE);
-        orderedExecutor = new OrderedExecutor<>();
-
-        serverEventDispatcher.addListener(new BuildServerAdapter() {
-
-            @Override
-            public void serverStartup() {
-                replaySavedEvents();
-            }
-
-            @Override
-            public void serverShutdown() {
-                shutdown();
-            }
-        });
-    }
-
-    public void publish(AsyncEvent event) {
-        for (AsyncEventListener listener : listeners.getOrDefault(event.getEventType(), Collections.emptyList())) {
-            try {
-                orderedExecutor.execute(new AsyncEventHandlingTask(listener, event), listener.getSyncKey());
-            } catch (IllegalStateException ex) {
-                LOG.error("Async event executor capacity is full. Event cannot be send: " + event, ex);
-            } catch (RejectedExecutionException ex) {
-                LOG.error("Cannot handle event " + event, ex);
-            }
-        }
+    public AsyncEventDispatcher(@SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+                                @Qualifier("coreAsyncEventDispatcher")
+                                jetbrains.buildServer.serverSide.impl.events.async.AsyncEventDispatcher delegate) {
+        myDelegate = delegate;
     }
 
     public void subscribe(String eventName, AsyncEventListener listener) {
-        synchronized (listeners) {
-            listeners.merge(eventName, new CopyOnWriteArrayList<>(new AsyncEventListener[]{listener}),
-                    (l1, l2) -> {
-                        if (listenerNameAlreadyRegistered(listener.getUniqName(), l1))
-                            throw new IllegalStateException(format("Listener with unique name \"%s\" is already registered for event %s.", listener.getUniqName(), eventName));
-                        l1.add(listener);
-                        return l1;
-                    });
-        }
+        myDelegate.subscribe(eventName, wrapListener(listener));
     }
 
     public void subscribe(List<String> eventNames, AsyncEventListener listener) {
-        synchronized (listeners) {
-            for (String eventName : eventNames) {
-                try {
-                    subscribe(eventName, listener);
-                } catch (IllegalStateException ex) {
-                    LOG.error(ex.getMessage());
-                }
-            }
-        }
+        myDelegate.subscribe(eventNames, wrapListener(listener));
     }
 
     public void unsubscribe(AsyncEventListener eventListener) {
-        synchronized (listeners) {
-            listeners.forEach((event, list) -> list.remove(eventListener));
-        }
+        myDelegate.unsubscribe(wrapListener(eventListener));
     }
 
-    public void shutdown() {
-        orderedExecutor.shutdown();
-        try {
-            saveUnprocessedEvents();
-        } catch (IOException ex) {
-            LOG.error("Failed to create file with unprocessed events.", ex.getMessage());
-        }
-    }
-
-    public boolean isTerminated() {
-        return orderedExecutor.isTerminated();
-    }
-
-    private void saveUnprocessedEvents() throws IOException {
-        synchronized (unprocessedEventsFilePath) {
-            if (Files.notExists(unprocessedEventsFilePath)) {
-                unprocessedEventsFilePath.getParent().toFile().mkdirs();
-                Files.createFile(unprocessedEventsFilePath);
+    private jetbrains.buildServer.serverSide.impl.events.async.AsyncEventListener wrapListener(AsyncEventListener listener) {
+        return new jetbrains.buildServer.serverSide.impl.events.async.AsyncEventListener() {
+            @Override
+            public void handle(AsyncEvent event) {
+                listener.handle(wrapEvent(event));
             }
-            try {
-                XStreamFile<List<TaskInfo>> file = new XStreamFile<>(unprocessedEventsFilePath.toFile(), new XStreamHolder());
-                file.serialize(mapToTaskInfo(orderedExecutor.getUnprocessedTasks()), true);
-            } catch (Throwable ex) {
-                LOG.error("Failed to save unprocessed events.", ex.getMessage());
+
+            @NotNull
+            private jetbrains.buildServer.webhook.async.events.AsyncEvent wrapEvent(AsyncEvent event) {
+                return new jetbrains.buildServer.webhook.async.events.AsyncEvent(event.getEventType(), event.getObjectId(), event.getProjectId());
             }
-        }
-    }
 
-    private void replaySavedEvents() {
-        synchronized (unprocessedEventsFilePath) {
-            if (Files.exists(unprocessedEventsFilePath)) {
-                try {
-                    XStreamFile<List<TaskInfo>> file = new XStreamFile<>(unprocessedEventsFilePath.toFile(), new XStreamHolder());
-                    List<TaskInfo> queue = file.deserialize();
-                    queue.forEach(this::executeTask);
-                } catch (Throwable ex) {
-                    LOG.error("Failed to replay saved events. " + ex.getMessage());
-                }
+            @Override
+            public int hashCode() {
+                return listener.hashCode();
             }
-        }
-    }
 
-    private void executeTask(TaskInfo task) {
-        for (AsyncEventListener listener : listeners.getOrDefault(task.getEvent().getEventType(), Collections.emptyList())) {
-            if (listener.getUniqName().equals(task.getRunnerName())) {
-                orderedExecutor.execute(new AsyncEventHandlingTask(listener, task.getEvent()), listener.getSyncKey());
-                return;
+            @Override
+            public boolean equals(Object obj) {
+                return listener.equals(obj);
             }
-        }
-    }
 
-    private boolean listenerNameAlreadyRegistered(String listenerName, List<AsyncEventListener> list) {
-        return list.stream().anyMatch(l -> l.getUniqName().equals(listenerName));
-    }
+            @Override
+            public void handle(List<AsyncEvent> events) {
+                listener.handle(events.stream().map(this::wrapEvent).collect(Collectors.toList()));
+            }
 
-    @NotNull
-    private List<TaskInfo> mapToTaskInfo(List<AsyncEventHandlingTask> tasks) {
-        return tasks.stream()
-                .map(t -> new TaskInfo(t.getListener().getUniqName(), t.getEvent()))
-                .collect(Collectors.toList());
-    }
+            @Override
+            public Object getSyncKey() {
+                return listener.getSyncKey();
+            }
 
-    static class TaskInfo {
-        private final AsyncEvent event;
-        private final String runnerName;
-
-        public TaskInfo(@NotNull String runnerName, @NotNull AsyncEvent event) {
-            this.event = event;
-            this.runnerName = runnerName;
-        }
-
-        public AsyncEvent getEvent() {
-            return event;
-        }
-
-        public String getRunnerName() {
-            return runnerName;
-        }
+            @NotNull
+            @Override
+            public String getUniqName() {
+                return listener.getUniqName();
+            }
+        };
     }
 }
